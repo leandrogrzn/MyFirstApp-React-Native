ce418a74d17ce8d99bfa4d8602aea1fc245a0464
leandrogrzn <hekoej@gmail.com>
2024-06-26 16:36:53 -0500

    personalizacion pressable

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Pressable, StyleSheet, Text, View } from "react-native"
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native"
 
 export const CounterScreen = () => {
 
@@ -15,8 +15,11 @@ export const CounterScreen = () => {
         <Pressable
             onPress={ () => increseBy(+1) }
             onLongPress={ () => setCount(0) }
+            style={ ({ pressed }) => [styles.button, pressed && styles.buttonPressed] }
         >
-        <Text>+1</Text>
+        <Text style={{ 
+            color: Platform.OS === 'android' ? 'white' : '#4746AB' 
+        }}>Incrementar</Text>
         </Pressable>
     </View>
   )
@@ -32,5 +35,14 @@ const styles = StyleSheet.create({
         fontSize: 80,
         color: 'black',
         fontWeight: '300'
+    },
+    button: {
+        backgroundColor: Platform.OS === 'android' ? '#5856D6': 'white',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 10,
+    },
+    buttonPressed: {
+        backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white',
     }
 })
