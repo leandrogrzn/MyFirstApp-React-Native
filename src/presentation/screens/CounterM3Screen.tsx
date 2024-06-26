3989adbe3e2f7d693cf7a2849269a8b93b311cdb
leandrogrzn <hekoej@gmail.com>
2024-06-26 18:59:19 -0500

    iconos en react native

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { View, Text, StyleSheet } from "react-native";
 import { globalstyles } from "../theme/global.styles";
 import { FAB } from 'react-native-paper';
+import Icon from "react-native-vector-icons/Ionicons";
 
 
 export const CounterM3Screen = () => {
@@ -11,11 +12,15 @@ export const CounterM3Screen = () => {
   return (
     <View style={ globalstyles.centerContainer }>
         <Text style={ globalstyles.title }>{ count }</Text>
+
+        <Icon name="game-controller-outline" size={ 35 } />
+
         <FAB
             label="+1"
             onPress={ () => setCount(count + 1) }
             onLongPress={ () => setCount(0) }
             style={ globalstyles.fab }
+            // icon={ () => <Icon name="game-controller-outline" size={ 20 } /> }
         />
     </View>
   )
