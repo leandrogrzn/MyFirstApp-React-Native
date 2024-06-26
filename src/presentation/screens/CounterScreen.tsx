8a99d58d992f8b2667d33394c0a926411b77f76c
leandrogrzn <hekoej@gmail.com>
2024-06-26 17:27:22 -0500

    componente personalizado primarybutton

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,26 +1,21 @@
 import { useState } from "react"
-import { Platform, Pressable, StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
+import { PrimaryButton } from "../components"
 
 export const CounterScreen = () => {
 
-    const [count, setCount] = useState(10)
-
-    const increseBy = (value: number ) => {
-        setCount( count + value)
-    }
+    const [count, setCount] = useState(10);
 
   return (
     <View style={styles.container}>
         <Text style={styles.title}>{ count }</Text>
-        <Pressable
-            onPress={ () => increseBy(+1) }
-            onLongPress={ () => setCount(0) }
-            style={ ({ pressed }) => [styles.button, pressed && styles.buttonPressed] }
-        >
-        <Text style={{ 
-            color: Platform.OS === 'android' ? 'white' : '#4746AB' 
-        }}>Incrementar</Text>
-        </Pressable>
+
+        <PrimaryButton
+            label='Incrementar'
+            onPress = { () => setCount(count + 1) }
+            onLongPress = { () => setCount(0) }
+        />
+
     </View>
   )
 }
@@ -36,13 +31,4 @@ const styles = StyleSheet.create({
         color: 'black',
         fontWeight: '300'
     },
-    button: {
-        backgroundColor: Platform.OS === 'android' ? '#5856D6': 'white',
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        borderRadius: 10,
-    },
-    buttonPressed: {
-        backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white',
-    }
 })
